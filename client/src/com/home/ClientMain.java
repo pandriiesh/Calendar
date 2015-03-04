c49a233d3f13c2f9424995956b92a77aaa707c2e
Pavlo <pasha_andriesh@mail.ru>
2015-03-04 20:55:52 +0200

    added client implementation

---
+++ client/src/com/home/ClientMain.java
@@ -0,0 +1,29 @@
+package com.home;
+
+import com.home.common.Event;
+import com.home.service.CalendarService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.rmi.RemoteException;
+import java.util.logging.Logger;
+
+public class ClientMain {
+
+    public static final Logger logger = Logger.getAnonymousLogger();
+
+    public static void main(String[] args) throws RemoteException {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("clientApplicationContext.xml");
+        CalendarService service = (CalendarService) context.getBean("calendarService");
+
+        String[] reservedCalendarNames = {"New Year", "Meeting10", "code review"};
+
+        for (String name : reservedCalendarNames)
+            service.addEvent(new Event.Builder().title(name).description(name).build());
+
+        for (String name : reservedCalendarNames)
+            logger.info("Created event in data store: " + service.searchEvent(name));
+
+    }
+}
